fe5a282a5cf001b5b3b6b73921eb9c51532a3ace
Otávio Lins <otavioslins97@gmail.com>
2022-01-21 16:44:16 -0300

    Pegando com uma gambirra

--- app/models/project.rb
+++ app/models/project.rb
@@ -22,13 +22,9 @@ class Project
         projects << Project.new({ title: d["project"]["title"], description: d["project"]["description"],
                                 skills: d["project"]["skills"], hour_value: d["project"]["hour_value"],
                                 date_limit: d["project"]["date_limit"], work_regimen: d["project"]["work_regimen"],
-                                status: d["project"]["status"], user: d["user"]["email"] })
+                                status: d["project"]["status"], user: d["project"]["user"]["email"] })
       end
     end
     return projects
   end
 end
-
-
-#[{:project=>{:id=>225, :title=>"Sistema de aluguel de imóveis", :description=>"Projeto que visa criar uma aplicação para oferecer imóveis alugáveis em todo o estado de São Paulo", :skills=>"Conhecimento em Rails, Web Design e segurança", :hour_value=>"300.0", :date_limit=>"2022-02-09", :work_regimen=>"remote", :status=>"open", :user=>{:email=>"otavio@user.com"}}}, 
-#{:project=>{:id=>226, :title=>"Sistema de aluguel de carros", :description=>"Projeto que visa criar uma aplicação para oferecer carros alugáveis em todo o estado de São Paulo", :skills=>"Conhecimento em Rails, Web Design e segurança", :hour_value=>"300.0", :date_limit=>"2022-01-30", :work_regimen=>"remote", :status=>"open", :user=>{:email=>"otavio@user.com"}}}]
